fd6a6c9255ee3744b9bc98058839f349f981704e
Kim woo jong <skekdnwhd@gmail.com>
2023-06-10 03:43:33 +0900

    item 7 예제 및 문서

---
+++ effec/src/main/java/org/example/chapter02/item07/EmptyStackException.java
@@ -0,0 +1,5 @@
+package org.example.chapter02.item07;
+
+// (36쪽의 Stack 코드에서 던지는 예외)
+public class EmptyStackException extends IllegalStateException {
+}
---
+++ effec/src/main/java/org/example/chapter02/item07/reference/BigObject.java
@@ -0,0 +1,4 @@
+package org.example.chapter02.item07.reference;
+
+public class BigObject {
+}
