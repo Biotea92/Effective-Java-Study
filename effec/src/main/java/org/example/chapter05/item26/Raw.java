bed7cd7cfb3a470f3fe9ed25bf6ee28e4de8ac32
kimwoojong <skekdnwhd@gmail.com>
2023-06-27 17:32:13 +0900

    chapter5 예제 및 문서

--- effec/src/main/java/org/example/chapter05/item26/Raw.java
+++ effec/src/main/java/org/example/chapter05/item26/Raw.java
@@ -2,6 +2,7 @@ package org.example.chapter05.item26;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 // 코드 26-4 런타임에 실패한다. - unsafeAdd 메서드가 로 타입(List)을 사용 (156-157쪽)
 public class Raw {
@@ -13,6 +14,21 @@ public class Raw {
 
     private static void unsafeAdd(List list, Object o) {
         list.add(o);
+    }
+
+    static int numElementsInCommon(Set<?> s1, Set<?> s2) {
+        int result = 0;
+        for (Object o1 : s1)
+            if (s2.contains(o1))
+                result++;
+        return result;
+    }
+
+    public void setExample(Object o) {
+        if (o instanceof Set) {
+            Set<?> s = (Set<?>) o;
+            // ...
+        }
     }
 }
 
