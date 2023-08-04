faa184172f6ae7bb67ac876281c6ff7ba5e05aef
kimwoojong <skekdnwhd@gmail.com>
2023-08-04 09:04:49 +0900

    chapter 9 예제

---
+++ effec/src/main/java/org/example/chapter09/item61/Unbelievable.java
@@ -0,0 +1,11 @@
+package org.example.chapter09.item61;
+
+// 코드 61-3 기이하게 동작하는 프로그램 - 결과를 맞혀보자! (360쪽)
+public class Unbelievable {
+    static Integer i;
+
+    public static void main(String[] args) {
+        if (i == 42)
+            System.out.println("믿을 수 없군!");
+    }
+}
