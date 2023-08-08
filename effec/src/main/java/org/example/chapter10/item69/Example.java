c065d664acb856fe1b94b1af8aaa7f45b9344202
kimwoojong <skekdnwhd@gmail.com>
2023-08-08 17:51:07 +0900

    item 69 정리

---
+++ effec/src/main/java/org/example/chapter10/item69/Example.java
@@ -0,0 +1,16 @@
+package org.example.chapter10.item69;
+
+public class Example {
+
+    public static void main(String[] args) {
+//        try {
+//            int i = 0;
+//            while (true)
+//                range[i++].climb();
+//        } catch (ArrayIndexOutOfBoundsException e) {
+//        }
+//
+//        for (Mountain m : range)
+//            m.climb();
+    }
+}
